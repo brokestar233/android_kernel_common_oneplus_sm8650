brokestar233/android_kernel_common_oneplus_sm8750

[HACK]module: Add module intercept and overlay mechanism
This commit introduces a mechanism to intercept and replace specific
kernel modules with embedded versions compiled directly into the kernel
image. The primary goals are:

- Ensure consistent use of specific module versions
- Prevent loading of external modules when desired

Key components:

1. `Documentation/module_intercept.txt`: New documentation describing
   the feature, usage, and testing instructions.

2. `kernel/module/module_overlay/`:
   - `Makefile`: Builds overlay support objects and generates
     `overlay_files.c` from `.ko` files in `modules/`.
   - `convert_overlay.sh`: Build script that converts `.ko` files into
     C byte arrays using `xxd`, sanitizes output, extracts correct
     lengths via `_len` parsing, and generates `overlay_file_list[]`.
   - `overlay_files.h`: Defines `struct overlay_file` and public
     intercept APIs.
   - `module_intercept.c`: Implements `should_intercept_module()` and
     `intercept_module_load()` to find and replace module data during
     `load_module()`.

3. `kernel/module/main.c`: Integrates interception logic into
   `load_module()` before signature checks and early module validation.
   On match, replaces `info->hdr` with embedded data via `vmalloc()` and
   `memcpy()`, then re-runs `module_sig_check()`.

4. `kernel/module/Makefile`: Adds `module_overlay/` subdirectory to
   build.

5. Example embedded modules:
   - `cpufreq_bouncing.ko`
   - `oplus_bsp_omrg.ko`

Usage:
- Place desired `.ko` files in `kernel/module/module_overlay/modules/`
- Rebuild kernel; matching modules will be automatically embedded
- Interception is 1:1 by module name (without `.ko`)
- Success logged via `pr_info("module_overlay: %s replaced...")`

The implementation is non-intrusive and only activates when a matching
embedded module is found. No changes to existing module loading paths
occur otherwise.

Change-Id: Ie6a80b05fb9b66d39b77aa49b49462cfc1dc4ae7
Signed-off-by: brokestar23

kernel/module/main.c
@@ -59,6 +59,7 @@
 #include <linux/debugfs.h>
 #include <uapi/linux/module.h>
 #include "internal.h"
+#include "module_overlay/overlay_files.h"
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/module.h>
@@ -2942,6 +2943,43 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	err = elf_validity_cache_copy(info, flags);
 	if (err)
 		goto free_copy;
+
+	/****************************************************************/
+	/*                HACK FOR MODULE INTERCEPTION                  */
+	/****************************************************************/
+	if (should_intercept_module(info->name)) {
+		char name_buf[MODULE_NAME_LEN];
+    	const char *name = name_buf;
+
+		/* 提前保存模块名,防止后续释放数据后日志输出错误的模块名 */
+    	strscpy(name_buf, info->name, sizeof(name_buf));
+
+		/*
+		 * 模块名匹配成功，执行替换。
+		 */
+		if (!intercept_module_load(info, name)) {
+			pr_err("Failed to intercept module %s\n", name);
+			err = -EINVAL;
+			goto free_copy;
+		}
+
+		pr_info("Module %s intercepted, re-running signature check\n", name);
+		err = module_sig_check(info, flags);
+		if (err) {
+			pr_err("Module %s: signature check failed after intercept!\n", name);
+			goto free_copy;
+		}
+		
+		/* 重新检查模块并缓存模块信息 */
+		err = elf_validity_cache_copy(info, flags);
+		if (err) {
+			pr_err("Module %s: ELF validity check failed after intercept!\n", name);
+			goto free_copy;
+		}
+	}
+	/****************************************************************/
+	/*                 END OF MODULE INTERCEPTION                   */
+	/****************************************************************/
 
 	err = early_mod_check(info, flags);
 	if (err)

brokestar233/android_kernel_common_oneplus_sm8650

kernel/module/main.c
@@ -56,6 +56,7 @@
 #include <linux/cfi.h>
 #include <uapi/linux/module.h>
 #include "internal.h"
+#include "module_overlay/overlay_files.h"
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/module.h>
@@ -2774,6 +2775,51 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	 * in the info structure.
 	 */
 	err = setup_load_info(info, flags);
+
+	/****************************************************************/
+	/*                HACK FOR MODULE INTERCEPTION                  */
+	/****************************************************************/
+	if (should_intercept_module(info->name)) {
+		char name_buf[MODULE_NAME_LEN];
+    	const char *name = name_buf;
+
+		/* 提前保存模块名,防止后续释放数据后日志输出错误的模块名 */
+    	strscpy(name_buf, info->name, sizeof(name_buf));
+
+		/*
+		 * 模块名匹配成功，执行替换。
+		 */
+		if (!intercept_module_load(info, name)) {
+			pr_err("Failed to intercept module %s\n", name);
+			err = -EINVAL;
+			goto free_copy;
+		}
+
+		pr_info("Module %s intercepted, re-running signature check\n", name);
+		err = module_sig_check(info, flags);
+		if (err) {
+			pr_err("Module %s: signature check failed after intercept!\n", name);
+			goto free_copy;
+		}
+		
+		/* 重新检查模块并缓存模块信息 */
+		err = elf_validity_check(info);
+		if (err) {
+			pr_err("Module %s: ELF validity check failed after intercept!\n", name);
+			goto free_copy;
+		}
+
+		err = setup_load_info(info, flags);
+		if (err) {
+			pr_err("Module %s: setup_load_info failed after intercept!\n", name);
+			goto free_copy;
+		}
+	}
+	/****************************************************************/
+	/*                 END OF MODULE INTERCEPTION                   */
+	/****************************************************************/
+
+	err = early_mod_check(info, flags);
 	if (err)
 		goto free_copy;
 